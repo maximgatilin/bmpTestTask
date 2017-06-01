83e947f7718c73ab76947ba337f0b0250aef1744
Maxim <gatilin@teamidea.ru>
2017-06-01 10:17:30 +0300

    Create table

--- src/App.js
+++ src/App.js
@@ -1,18 +1,40 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import exhibits from './data/exhibits.json'
 
 class App extends Component {
+   constructor(props) {
+    super(props);
+    this.state = {exhibits: exhibits};
+  }
+
+  componentDidMount() {
+   
+  }
+
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className="container">
+        <table className="table">
+         <caption>Экспонаты</caption>
+          <thead>
+            <tr>
+              <th>Название</th>
+              <th>Место создания</th>
+              <th>Организация</th>
+              <th>Описание</th>
+            </tr>
+          </thead>
+          <tbody>
+            { this.state.exhibits.map((exhibit, index) =>
+              <tr key={index}>
+                <td>{exhibit.name}</td>
+                <td>{exhibit.city}, {exhibit.country}</td>
+                <td>{exhibit.organization}</td> 
+                <td>{exhibit.description || "Описание отсутствует"}</td>
+              </tr>)
+            }
+          </tbody>
+        </table>
       </div>
     );
   }
